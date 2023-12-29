e55f5353aa08e2cad43bac7ba084d43a09e588ea
Luigi <lugminsinlag@gmail.com>
2023-12-29 21:35:06 +0100

    feat: Trying to make vim yank to linux clipboard and adding some comments on what some settings do.

--- lua/lug/set.lua
+++ lua/lug/set.lua
@@ -1,6 +1,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tab config
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -10,6 +11,7 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Undodir config
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -26,4 +28,9 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
+-- Vertical bar at 80ch
 vim.opt.colorcolumn = "80"
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
