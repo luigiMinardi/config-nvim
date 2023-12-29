41e3560ebd29bf73201b3334178e11ab980620c0
Luigi <lugminsinlag@gmail.com>
2023-12-29 22:10:02 +0100

    feat: Removing LSP-Zero and using the kickstart.nvim LSP configuration instead
    Improving the comments on the lazy plugin installation file and also adding some good plugins.

--- lua/lug/lazy.lua
+++ lua/lug/lazy.lua
@@ -1,26 +1,40 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+    -- Fuzzy Finder (files, lsp, etc)
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        -- or                            , branch = '0.1.x',
-        dependencies = {'nvim-lua/plenary.nvim'}
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+            -- Only load if `make` is available. Make sure you have the system
+            -- requirements installed.
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
     },
 
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-
     {
+        -- Color theme
         'rose-pine/neovim',
         name = 'rose-pine',
         config = function()
@@ -28,7 +42,74 @@ local plugins = {
         end
     },
 
+
+    -- Highlight, edit, and navigate code
     {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+
+    'nvim-treesitter/playground',
+
+    "nvim-treesitter/nvim-treesitter-context",
+
+    -- Jump files
+    'theprimeagen/harpoon',
+
+    -- Better undo
+    'mbbill/undotree',
+
+    -- Git related plugins
+    'tpope/vim-fugitive',
+
+    {
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = 'ibl',
+        opts = {},
+    },
+
+    {
+        -- LSP Configuration & Plugins
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            { 'williamboman/mason.nvim', config = true },
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim', opts = {} },
+
+            -- Additional lua configuration, makes nvim stuff amazing!
+            'folke/neodev.nvim',
+        },
+    },
+
+    {
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
+        },
+    },
+
+    {
+        -- Diagnostics, Quickfix, etc
         "folke/trouble.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
         config = function()
@@ -39,47 +120,6 @@ local plugins = {
             }
         end
     },
-
-    'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-
-    'nvim-treesitter/playground',
-
-    "nvim-treesitter/nvim-treesitter-context",
-
-    'theprimeagen/harpoon',
-
-    'mbbill/undotree',
-
-    'tpope/vim-fugitive',
-
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {
-                'williamboman/mason.nvim', 
-                build = function() 
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            }, -- Optional 
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
 }
 
 require("lazy").setup(plugins, {})
