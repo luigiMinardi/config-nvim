9bb7e681938983747f0a8e34a340bd22d00285e2
Luigi <lugminsinlag@gmail.com>
2024-01-12 23:16:43 +0100

    feat: adding keybind to remove executable perm from a file and also removing executable perms from all files of this repo since they dont need.

--- lua/lug/remap.lua
+++ lua/lug/remap.lua
@@ -31,4 +31,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Turn file into executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>-x", "<cmd>!chmod -x %<CR>", { silent = true })
 
