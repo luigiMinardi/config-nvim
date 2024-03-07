e64a8b09a2d6b6bf858e01affef37b126c7948f6
Luigi <lugminsinlag@gmail.com>
2024-03-07 20:27:21 +0100

    fix: Fixing duplicate remap and adding required setup to make harpoon work correctly on v2.

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,9 @@
 local harpoon = require("harpoon")
 
+-- REQUIRED
+harpoon:setup({})
+-- REQUIRED
+
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
