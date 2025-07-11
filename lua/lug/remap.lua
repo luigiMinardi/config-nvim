274dcacead3881cfcd02e2b35fa9528cff746c08
Luigi Minardi <lugminsinlag@gmail.com>
2025-07-11 11:41:40 +0200

    fix: Harpoon <C-l> keybind conflict with netwr's NetwrRefresh default keybind.

--- lua/lug/remap.lua
+++ lua/lug/remap.lua
@@ -20,11 +20,11 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Beign able to yank to your clipboard | next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Don't yank the deletion
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Replace current word globally
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -33,3 +33,24 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>-x", "<cmd>!chmod -x %<CR>", { silent = true })
 
+-- Remove NetrwRefresh <C-l> bind and dont rebind it since we dont use it
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = 'netrw',
+    callback = function()
+        vim.schedule(function()
+            vim.keymap.set("n", "<C-l>", function() end, {
+                buffer = true
+            })
+            vim.keymap.del("n", "<C-l>", {
+                buffer = true
+            })
+            vim.keymap.set("n", "<C-r>", "<Plug>NetrwRefresh", {
+                buffer = true,
+                silent = true,
+                noremap = true,
+                -- unique = true
+            })
+        end)
+    end
+})
